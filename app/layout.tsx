28eb1d5748736ef8713d71ed45eb87a366072ed1
Sukhtumur <it@apex.mn>
2025-05-09 12:06:47 +0800

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,13 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
   openGraph: {
     title: "CoachLab Mongolia",
     description:
       "CoachLab Mongolia is a platform for coaches and athletes to connect and share knowledge.",
     images: [
       {
-        url: "/favicon.ico",
+        url: "/Logo.png",
         width: 32,
         height: 32,
         alt: "lab Logo",
